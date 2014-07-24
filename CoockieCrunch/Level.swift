c7055bd883845e21cfa5e325071e910bc996741c
Dariusz Cieśla <dariusz.ciesla@MacBook-Air-IT.local>
2014-07-24 15:33:38 +0200

    Tiles

--- CoockieCrunch/Level.swift
+++ CoockieCrunch/Level.swift
@@ -5,11 +5,38 @@ let NumRows = 9
 
 class Level {
     let cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)  // private
+    let tiles = Array2D<Tile>(columns: NumColumns, rows: NumRows)  // private
+    
+    init(filename: String) {
+        // 1
+        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename) {
+            // 2
+            if let tilesArray: AnyObject = dictionary["tiles"] {
+                // 3
+                for (row, rowArray) in enumerate(tilesArray as [[Int]]) {
+                    // 4
+                    let tileRow = NumRows - row - 1
+                    // 5
+                    for (column, value) in enumerate(rowArray) {
+                        if value == 1 {
+                            tiles[column, tileRow] = Tile()
+                        }
+                    }
+                }
+            }
+        }
+    }
     
     func cookieAtColumn(column: Int, row: Int) -> Cookie? {
         assert(column >= 0 && column < NumColumns)
         assert(row >= 0 && row < NumRows)
         return cookies[column, row]
+    }
+    
+    func tileAtColumn(column: Int, row: Int) -> Tile? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return tiles[column, row]
     }
     
     func shuffle() -> Set<Cookie> {
@@ -22,16 +49,17 @@ class Level {
         // 1
         for row in 0..<NumRows {
             for column in 0..<NumColumns {
+                if tiles[column, row] {
+                    // 2
+                    var cookieType = CookieType.random()
                 
-                // 2
-                var cookieType = CookieType.random()
+                    // 3
+                    let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                    cookies[column, row] = cookie
                 
-                // 3
-                let cookie = Cookie(column: column, row: row, cookieType: cookieType)
-                cookies[column, row] = cookie
-                
-                // 4
-                set.addElement(cookie)
+                    // 4
+                    set.addElement(cookie)
+                }
             }
         }
         return set
