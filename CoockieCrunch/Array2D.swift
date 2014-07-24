cd74a83f37b8e341f33c2e15e445b258a5526ecb
Dariusz Cieśla <dariusz.ciesla@MacBook-Air-IT.local>
2014-07-24 14:52:51 +0200

    base classes

---
+++ CoockieCrunch/Array2D.swift
@@ -0,0 +1,20 @@
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>  // private
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows*columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
